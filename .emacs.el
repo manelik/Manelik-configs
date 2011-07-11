5bd2cc216920c259aa257252008da3d0acc7a208
Jose Manuel Torres <josemanuel.tch@gmail.com>
2011-07-11 02:33:49 -0500

    Emacs auctex UUUU

--- .emacs.el
+++ .emacs.el
@@ -13,3 +13,13 @@
             (set-face-bold-p face (not dark-background))))
      (face-list)))
   (setq dark-background (not dark-background)))
+
+
+;; Auctex stuff
+
+(setq TeX-parse-self t) ; Enable parse on load.
+(setq TeX-auto-save t) ; Enable parse on save.
+
+;; Start with dark mode activated
+
+(toggle-dark-background)
